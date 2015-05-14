bb900e46a88f5739a45a92ef8efcf86fe071d6e9
Amanda <amanda96campos@gmail.com>
2015-05-14 13:43:28 -0300

    commit 01

---
+++ MC03Tests/MC03Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MC03Tests.swift
+//  MC03Tests
+//
+//  Created by Amanda Guimaraes Campos on 14/05/15.
+//  Copyright (c) 2015 Amanda Guimaraes Campos. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MC03Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
