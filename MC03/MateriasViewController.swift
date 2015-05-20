3ec5dd3496246da191c71ed1cde285ad8e91ad5a
Amanda <amanda96campos@gmail.com>
2015-05-20 18:03:35 -0300

    busca coreData

--- MC03/MateriasViewController.swift
+++ MC03/MateriasViewController.swift
@@ -7,10 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
-class MateriasViewController: UIViewController {
 
+class MateriasViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var buttonAdcMateria: UIBarButtonItem!
     @IBOutlet weak var tableView: UITableView!
+    
+    
+    var materia: Array<Materia>?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        materia = MateriaManager.sharedInstance.Materia()
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return materia!.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let celula = tableView.dequeueReusableCellWithIdentifier("celMateria") as? MateriaTableViewCell
+        
+        //        let materia = materias[indexPath.row]
+        celula!.labelNome?.text = materia?[indexPath.row].nomeMateria
+        
+        return celula!
+    }
+    
     
 }
