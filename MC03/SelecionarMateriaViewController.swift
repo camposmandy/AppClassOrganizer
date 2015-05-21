c950d7f5398ae5ce22f1655349a00f45cdd375ac
Amanda <amanda96campos@gmail.com>
2015-05-21 16:22:06 -0300

    Checkmark de matérias.

--- MC03/SelecionarMateriaViewController.swift
+++ MC03/SelecionarMateriaViewController.swift
@@ -8,6 +8,43 @@
 
 import UIKit
 
-class SelecionarMateriaViewController: UIViewController {
+class SelecionarMateriaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var materiasSelecionadas: Array<Materia>?
+    var select: NSIndexPath?
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        materiasSelecionadas = MateriaManager.sharedInstance.Materia()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return materiasSelecionadas!.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if(select != nil){
+            var celula = tableView.cellForRowAtIndexPath(self.select!)
+            celula?.accessoryType = .None
+        }
+        var celula2 = tableView.cellForRowAtIndexPath(indexPath)
+        celula2?.accessoryType = .Checkmark
+        
+        self.select = indexPath
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("celMateria", forIndexPath: indexPath) as! SelecionarMateriaTableViewCell
+        cell.lblMateria.text = materiasSelecionadas?[indexPath.row].nomeMateria
+        return cell
+    }
 }
