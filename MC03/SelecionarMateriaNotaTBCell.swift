0a7cda87ec467f2bfa7c908881f4e895c0acfa5c
JoaoMarcos <jmra10@gmail.com>
2015-05-25 14:48:02 -0300

    Commit2(Amanda)

---
+++ MC03/SelecionarMateriaNotaTBCell.swift
@@ -0,0 +1,25 @@
+//
+//  SelecionarMateriaNotaTBCell.swift
+//  MC03
+//
+//  Created by João Marcos on 25/05/15.
+//  Copyright (c) 2015 Amanda Guimaraes Campos. All rights reserved.
+//
+
+import UIKit
+
+class SelecionarMateriaNotaTBCell: UITableViewCell {
+
+    @IBOutlet weak var lblNomeMateria: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MC03/SelecionarMateriaNotaTBVC.swift
+++ MC03/SelecionarMateriaNotaTBVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SelecionarMateriaNotaTBVCTableViewController: UITableViewController {
+class SelecionarMateriaNotaTBVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
