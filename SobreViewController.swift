c99936508080c3aebed3c133172161565cc71eeb
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-05-25 14:23:45 -0300

    View Sobre.

---
+++ SobreViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SobreViewController.swift
+//  MC03
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 25/05/15.
+//  Copyright (c) 2015 Amanda Guimaraes Campos. All rights reserved.
+//
+
+import UIKit
+
+class SobreViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logoApp: UIImageView!
+    @IBOutlet weak var sobreApp: UITextView!
+    
+
+}
