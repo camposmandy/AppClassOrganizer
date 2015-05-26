56ee3d71ba8da70251782187044d2f38a8ca0236
Amanda <amanda96campos@gmail.com>
2015-05-26 14:19:10 -0300

    faltas

--- MC03/FaltasViewController.swift
+++ MC03/FaltasViewController.swift
@@ -8,8 +8,47 @@
 
 import UIKit
 
-class FaltasViewController: UIViewController {
+class FaltasViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-
+    var mat: Array<Materia>?
+    
+    
+    @IBAction func buttonMais(sender: AnyObject) {
+    
+        
+        
+    }
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mat = MateriaManager.sharedInstance.Materia()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mat!.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var celula =  tableView.dequeueReusableCellWithIdentifier("celFaltas", forIndexPath: indexPath) as? FaltaTableViewCell
+
+        celula!.lblMateria.text = mat?[indexPath.row].nomeMateria
+        celula!.lblPercentualFalta.text = "\(mat?[indexPath.row].faltas)"
+        
+        return celula!
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        tableView.reloadData()
+    }
 }
